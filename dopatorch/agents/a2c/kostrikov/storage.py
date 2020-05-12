9b88e0ffb417f53786b55fdfea52deab7bd5599f
im-ant <anthony.gx.chen@gmail.com>
2020-05-12 17:20:44 -0400

    cleaning up code format

--- dopatorch/agents/a2c/kostrikov/storage.py
+++ dopatorch/agents/a2c/kostrikov/storage.py
@@ -1,3 +1,16 @@
+# =============================================================================
+# Rollout storage
+#
+# Inspired by and adapted from the policy networks in:
+# repository: https://github.com/ikostrikov/pytorch-a2c-ppo-acktr-gail
+# File: blob/master/a2c_ppo_acktr/storage.py
+#
+# NOTE: horrendously lacking in comments from source.
+#
+# Original author repository: ikostrikov
+# Adapted by: Anthony G. Chen
+# =============================================================================
+
 import torch
 from torch.utils.data.sampler import BatchSampler, SubsetRandomSampler
 
@@ -9,13 +22,18 @@ def _flatten_helper(T, N, _tensor):
 class RolloutStorage(object):
     def __init__(self, num_steps, num_processes, obs_shape, action_space,
                  recurrent_hidden_state_size):
-        self.obs = torch.zeros(num_steps + 1, num_processes, *obs_shape)
-        self.recurrent_hidden_states = torch.zeros(
-            num_steps + 1, num_processes, recurrent_hidden_state_size)
+
+        self.obs = torch.zeros(num_steps + 1,
+                               num_processes,
+                               *obs_shape)
+        self.recurrent_hidden_states = torch.zeros(num_steps + 1,
+                                                   num_processes,
+                                                   recurrent_hidden_state_size)
         self.rewards = torch.zeros(num_steps, num_processes, 1)
         self.value_preds = torch.zeros(num_steps + 1, num_processes, 1)
         self.returns = torch.zeros(num_steps + 1, num_processes, 1)
         self.action_log_probs = torch.zeros(num_steps, num_processes, 1)
+
         if action_space.__class__.__name__ == 'Discrete':
             action_shape = 1
         else:
@@ -23,6 +41,7 @@ class RolloutStorage(object):
         self.actions = torch.zeros(num_steps, num_processes, action_shape)
         if action_space.__class__.__name__ == 'Discrete':
             self.actions = self.actions.long()
+
         self.masks = torch.ones(num_steps + 1, num_processes, 1)
 
         # Masks that indicate whether it's a true terminal state
@@ -33,6 +52,11 @@ class RolloutStorage(object):
         self.step = 0
 
     def to(self, device):
+        """
+        Send the RolloutStorage to device
+        :param device: 'cpu' or 'cuda'
+        :return:
+        """
         self.obs = self.obs.to(device)
         self.recurrent_hidden_states = self.recurrent_hidden_states.to(device)
         self.rewards = self.rewards.to(device)
@@ -45,6 +69,18 @@ class RolloutStorage(object):
 
     def insert(self, obs, recurrent_hidden_states, actions, action_log_probs,
                value_preds, rewards, masks, bad_masks):
+        """
+
+        :param obs:
+        :param recurrent_hidden_states:
+        :param actions:
+        :param action_log_probs:
+        :param value_preds:
+        :param rewards:
+        :param masks:
+        :param bad_masks:
+        :return:
+        """
         self.obs[self.step + 1].copy_(obs)
         self.recurrent_hidden_states[self.step +
                                      1].copy_(recurrent_hidden_states)
@@ -74,35 +110,41 @@ class RolloutStorage(object):
                 self.value_preds[-1] = next_value
                 gae = 0
                 for step in reversed(range(self.rewards.size(0))):
-                    delta = self.rewards[step] + gamma * self.value_preds[
-                        step + 1] * self.masks[step +
-                                               1] - self.value_preds[step]
-                    gae = delta + gamma * gae_lambda * self.masks[step +
-                                                                  1] * gae
-                    gae = gae * self.bad_masks[step + 1]
+                    delta = (self.rewards[step]
+                             + ((gamma * self.value_preds[step+1])
+                                * self.masks[step+1])
+                             - self.value_preds[step])
+                    gae = (delta + (gamma * gae_lambda
+                                    * self.masks[step+1] * gae))
+                    gae = gae * self.bad_masks[step+1]
                     self.returns[step] = gae + self.value_preds[step]
             else:
                 self.returns[-1] = next_value
                 for step in reversed(range(self.rewards.size(0))):
-                    self.returns[step] = (self.returns[step + 1] * \
-                        gamma * self.masks[step + 1] + self.rewards[step]) * self.bad_masks[step + 1] \
-                        + (1 - self.bad_masks[step + 1]) * self.value_preds[step]
+                    self.returns[step] = (
+                            ((self.returns[step+1] * gamma * self.masks[step+1])
+                             + self.rewards[step]) * self.bad_masks[step+1]
+                            + ((1 - self.bad_masks[step+1])
+                               * self.value_preds[step])
+                    )
         else:
             if use_gae:
                 self.value_preds[-1] = next_value
                 gae = 0
                 for step in reversed(range(self.rewards.size(0))):
-                    delta = self.rewards[step] + gamma * self.value_preds[
-                        step + 1] * self.masks[step +
-                                               1] - self.value_preds[step]
-                    gae = delta + gamma * gae_lambda * self.masks[step +
-                                                                  1] * gae
+                    delta = (self.rewards[step]
+                             + (gamma * self.value_preds[step+1]
+                                * self.masks[step+1])
+                             - self.value_preds[step])
+                    gae = (delta + (gamma * gae_lambda
+                                    * self.masks[step+1] * gae))
                     self.returns[step] = gae + self.value_preds[step]
             else:
                 self.returns[-1] = next_value
                 for step in reversed(range(self.rewards.size(0))):
-                    self.returns[step] = self.returns[step + 1] * \
-                        gamma * self.masks[step + 1] + self.rewards[step]
+                    self.returns[step] = ((self.returns[step+1]
+                                           * gamma * self.masks[step+1])
+                                          + self.rewards[step])
 
     def feed_forward_generator(self,
                                advantages,
@@ -194,8 +236,8 @@ class RolloutStorage(object):
             value_preds_batch = _flatten_helper(T, N, value_preds_batch)
             return_batch = _flatten_helper(T, N, return_batch)
             masks_batch = _flatten_helper(T, N, masks_batch)
-            old_action_log_probs_batch = _flatten_helper(T, N, \
-                    old_action_log_probs_batch)
+            old_action_log_probs_batch = _flatten_helper(T, N,
+                                                         old_action_log_probs_batch)
             adv_targ = _flatten_helper(T, N, adv_targ)
 
             yield obs_batch, recurrent_hidden_states_batch, actions_batch, \
